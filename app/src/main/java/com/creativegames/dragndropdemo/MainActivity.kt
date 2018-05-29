2f289d8ac8c7ae57317df867081ee5f661daf863
Telolahy <stephano.telolahy@gmail.com>
2018-05-29 18:33:32 +0200

    clean

--- app/src/main/java/com/creativegames/dragndropdemo/MainActivity.kt
+++ app/src/main/java/com/creativegames/dragndropdemo/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val contentView = findViewById<View>(android.R.id.content) as FrameLayout
+        val contentView = findViewById<View>(android.R.id.content) as ViewGroup
 
         val imageView = ImageView(this)
         imageView.setImageResource(R.mipmap.ic_launcher)
